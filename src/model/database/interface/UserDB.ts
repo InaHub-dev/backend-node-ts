679a9d6e9bc7dc3e4b1fd3dba2441b577017db9d
Bruno <bruno.3av@gmail.com>
2021-10-30 09:37:23 -0300

    user db interface fixed

--- src/model/database/interface/UserDB.ts
+++ src/model/database/interface/UserDB.ts
@@ -3,5 +3,16 @@ import { User } from "../../User";
 export default interface UserDB {
   createUser(user: User): Promise<unknown>;
   selectAllUsers(): Array<User> | Promise<unknown>;
+  selectUserByLogin(email: string, password: string): User | Promise<unknown>;
+  updateNameByEmail(
+    email: string,
+    password: string,
+    newName: string
+  ): User | Promise<unknown>;
+  deleteUser(
+    email: string,
+    password: string,
+    name: string
+  ): User | Promise<unknown>;
   // selectedUserByLogin(): User;
 }
